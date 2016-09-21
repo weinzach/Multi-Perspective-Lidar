3df9d3d0eb0550bee0ff91817eeb8d9624766fc6
Zachary Weingarten <zdog1230@yahoo.com>
2016-09-21 17:29:31 -0400

    Added slicing options to plot_scans

--- Plotting/plot_scans.py
+++ Plotting/plot_scans.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
-import read_scans as rs 
+import read_scans as rs
 import dbscan
 
 #fig = plt.figure()
@@ -11,6 +11,11 @@ f = 'lidardata.txt'
 
 scans = rs.read_scans(f)
 
+#Slicing Options with lower/upper bounds
+SLICING = False
+LOW_SLICE = 460
+UP_SLICE = 500
+
 processed_scans = rs.filter_scans(scans)
 
 xs = []
@@ -19,9 +24,16 @@ zs = []
 
 for scan in range(len(processed_scans)):
 	for point in range(len(processed_scans[scan])):
-		xs.append(processed_scans[scan][point][0])
-		ys.append(processed_scans[scan][point][1])
-		zs.append(processed_scans[scan][point][2])
+		#Slicing Test 9/21/2016
+		if(SLICING):
+			if((processed_scans[scan][point][2])<UP_SLICE)and((processed_scans[scan][point][2])>LOW_SLICE):
+				xs.append(processed_scans[scan][point][0])
+				ys.append(processed_scans[scan][point][1])
+				zs.append(processed_scans[scan][point][2])
+		else:
+			xs.append(processed_scans[scan][point][0])
+			ys.append(processed_scans[scan][point][1])
+			zs.append(processed_scans[scan][point][2])
 
 data = np.zeros((len(xs),3))
 for element in range(len(xs)):
@@ -33,4 +45,3 @@ processed_data = dbscan.cluster(data, 100, 5)
 #ax.scatter(xs,ys,zs, s= 2, marker='+')
 
 #plt.show()
-
