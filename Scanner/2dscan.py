3852f5677eef8d33b4293a1a54b38e9a2672a2f0
zdog1230 <zdog1230@yahoo.com>
2017-12-30 17:47:16 +0000

    Add new 2dscan file

--- Scanner/2dscan.py
+++ Scanner/2dscan.py
@@ -1,26 +1,21 @@
 import sys
 from rplidar import RPLidar
-import math
 
-# = '/dev/ttyUSB0'
-PORT_NAME = 'COM4'
+PORT_NAME = '/dev/ttyUSB0'
 
 def run():
-        lidar = RPLidar(PORT_NAME)
-        outfile = open('lidardata.txt', 'w')
-        try:
+	lidar = RPLidar(PORT_NAME)
+	outfile = open('lidardata.txt', 'w')
+	try:
                 print('Recording measurments... Press Crl+C to stop.')
-                for measurment in lidar.iter_measurments():
-                        if(measurment[3]!=0):
-                                x = math.cos(measurment[2])*measurment[3]
-                                y = math.sin(measurment[2])*measurment[3]
-                                line = str(x)+" "+str(y)
-                                outfile.write(line + '\n')
-        except KeyboardInterrupt:
-                print('Stopping')
-        lidar.stop()
-        lidar.disconnect()
-        outfile.close()
+		for measurment in lidar.iter_measurments():
+			line = '\t'.join(str(v) for v in measurment)
+			outfile.write(line + '\n')
+	except KeyboardInterrupt:
+		print('Stopping')
+	lidar.stop()
+	lidar.disconnect()
+	outfile.close()
 
 if __name__ == '__main__':
 	run()
